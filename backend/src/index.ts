27e08ddef7d1b51fe184ed6cc241da04489fcc5b
Arpit 'Nanu <arpit.verma.av@gmail.com>
2026-01-06 22:04:55 +0530

    zod-validation-for-signup-route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,20 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { PrismaNeon } from "@prisma/adapter-neon";
+import * as z from "zod";
+import { resolveCallback } from "hono/utils/html";
 
 export interface Env {
   DATABASE_URL: string;
 }
+
+const SignupSchema = z.object({
+  email: z.string(),
+  firstName: z.string().trim(),
+  lastName: z.string().trim(),
+  bio: z.string().max(250).optional(),
+  socialLinks: z.string().optional(),
+});
 
 const app = new Hono<{ Bindings: Env }>();
 
@@ -18,19 +28,40 @@ export const getPrisma = (env: Env) => {
 
 app.get("/", (c) => c.text("hono!"));
 
-app.post("/signup", async (c) => {
-  const body = await c.req.json();
-  const prisma = getPrisma(c.env);
-  await prisma.user.create({
-    data: {
-      email: body.email,
-      firstName: body.firstName,
-      lastName: body.lastName,
-      bio: body.bio,
-      socialLinks: body.socialLinks,
-    },
-  });
-  return c.json({ msg: "User Created" });
+app.post("/api/v1/signup", async (c) => {
+  try {
+    const body = await c.req.json();
+
+    const validInput = SignupSchema.safeParse(body);
+    if (!validInput.success) return console.error("invalid user information");
+    else {
+      const email = validInput.data?.email;
+      const prisma = getPrisma(c.env);
+      const userExists = await prisma.user.findUnique({
+        where: {
+          email: email,
+        },
+      });
+      if (userExists) {
+        return console.error("user already exists");
+      } else {
+        await prisma.user.create({
+          data: {
+            email: validInput.data?.email,
+            firstName: validInput.data?.firstName,
+            lastName: validInput.data?.lastName,
+            bio: validInput.data?.bio,
+            socialLinks: validInput.data?.socialLinks,
+          },
+        });
+        return c.json({ msg: "User Created" });
+      }
+    }
+  } catch (error) {
+    console.error("oops something went wrong");
+  } finally {
+    console.log("signup reached to database");
+  }
 });
 
 export default app;
