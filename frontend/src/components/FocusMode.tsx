6080e334baa9063dfc84164fa4c4897b029d3e79
Arpit 'Nanu <arpit.verma.av@gmail.com>
2026-05-03 13:51:28 +0530

    play-pause-completed-restart-remaining

--- frontend/src/components/FocusMode.tsx
+++ frontend/src/components/FocusMode.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { boolean } from "zod";
 const givenstring =
   "Durable Objects present a simpler paradigm: write a JavaScript class";
 
@@ -8,25 +7,36 @@ const words = givenstring.split(/\s+/).filter(boolean);
 export const FocusMode = () => {
   const [focusmode, setFocusmode] = useState(false);
   const [index, setIndex] = useState(0);
+
   useEffect(() => {
+    if (!focusmode) return;
     const id = setTimeout(() => {
-      if (!focusmode) return;
       setIndex((prev) => {
         if (prev >= words.length - 1) return prev; //for last word check
         return prev + 1;
       });
     }, 500);
-    if (index >= words.length - 1) {
-      setFocusmode(false);
-    }
+
     return () => clearTimeout(id); // clear old timer
   }, [focusmode, index]);
+
+  useEffect(() => {
+    if (index >= words.length - 1) {
+      setFocusmode(false);
+      //setIndex(0);
+    }
+  }, [index, focusmode]);
+
+  const handleFocsMode = () => {
+    if (index >= words.length - 1) {
+      setIndex(0);
+    }
+    setFocusmode((prev) => !prev);
+  };
+
   return (
     <div>
-      <button
-        className="cursor-pointer"
-        onClick={() => setFocusmode(!focusmode)}
-      >
+      <button className="cursor-pointer" onClick={handleFocsMode}>
         focusmode/stop
       </button>
       <div>{words[index]}</div>
